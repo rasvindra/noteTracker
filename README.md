15f1e668016ebd0d23e8afa2e0bcca45130b4762
rasvindra <rasvindra@hotmail.com>
2022-07-25 15:00:28 -0700

    created server.js file and added routes

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Note Taker Starter Code
+
+
+# Note Tracker
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+## Table of Contents
+* [Description](#description)
+* [Install](#install)
+* [Contribution](#contribution)
+* [Usage](#usage)
+* [Testing](#testing)
+* [Questions](#questions)
+* [License](#license)
+
+
+## Description
+A simple app to create,store, and delete notes using Node.js and LocalHost for the DB
+
+## Install
+npm packages fs path    express
+
+## Contribution
+NaN
+
+## Usage
+NaN
+
+## Testing
+NaN
+
+## Questions
+Please reach out to me though my GitHub or via E-mail with any questions or concerns you may have <br/>
+rasvindra@hotmail.com <br/>
+[github.com/rasvindra](https://github.com/rasvindra)
+
+## License
+ISC
+
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("./helpers/uuid")
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+// HTML ROUTES
+app.get("/", (req, res)=>
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+app.get("/notes", (req, res)=>
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+// API ROUTES
+app.get("/api/notes",(req,res)=>{
+    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+    res.json(notes);
+});
+// API route for adding/posting new items to DB
+app.post("/api/notes",(req,res)=>{
+    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+    let newNote = req.body;
+    newNote.id = uuid();
+    let updatedNotes = [...notes,newNote];
+    fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes))
+    res.json(updatedNotes);
+});
+
+// API ROUTE for delete method to remove items from DB
+app.delete("/api/notes/:id",(req,res)=>{
+    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+    let newNote = req.params.id 
+    let updatedNotes = [notes.splice(newNote,1)] // Splice to remove
+    fs.writeFileSync("./db/db.json". JSON.stringify(updatedNotes))
+    res.json(updatedNotes)
+
+
+});
+
+app.listen(PORT, () => 
+console.log(`App listening at http://localhost:${PORT}`)
+);
